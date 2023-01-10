37591aa48956bd1ba6d2efb9bd546f730e0c2121
Brittany Laura Catone <bcatone@outlook.com>
2023-01-10 13:15:48 -0500

    commit tree bd06797be888eb47d38de40a927452513b999316

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    # setter method
+    def name=(value)
+        @name = value
+    end
+
+    # getter method
+    def name
+        @name
+    end
+
+    def job=(value)
+        @job = value
+    end
+
+    def job
+        @job
+    end
 
 end
